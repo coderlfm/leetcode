dfb5b69d6663aaed7e751e7e50f3ae42198d1a13
茂茂 <375642570@qq.com>
2022-06-17 13:44:16 +0800

    feat: 新增 1528. 重新排列字符串

---
+++ 题库/1528. 重新排列字符串.ts
@@ -0,0 +1,10 @@
+export {};
+
+function restoreString(s: string, indices: number[]): string {
+  let arr: string[] = [];
+
+  for (let i = 0; i < s.length; i++) {
+    arr[indices[i]] = s[i];
+  }
+  return arr.join();
+}
